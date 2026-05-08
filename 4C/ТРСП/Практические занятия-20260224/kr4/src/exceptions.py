0d2426d4e8fdf94a20c1b4f8a07807ef921e351d
vladcheck <valdemarcheck@yandex.ru>
2026-05-08 20:38:04 +0300

    feat: remove useless folder

--- 4C/ТРСП/Практические занятия-20260224/kr4/src/exceptions.py
+++
@@ -1,7 +0,0 @@
-class CustomExceptionA(Exception):
-    def __init__(self, name: str) -> None:
-        self.name: str = name
-
-class CustomExceptionB(Exception):
-    def __init__(self, resource_id: int) -> None:
-        self.resource_id: int = resource_id
